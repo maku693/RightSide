1c07d6bc687909601352c9d7b011d0046ffec50a
Shota HAMADA <maku693@outlook.com>
2018-02-09 23:24:35 +0900

    Initial Commit

---
+++ DirectoryViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DirectoryViewer
+//
+//  Created by Shota HAMADA on 2018/02/09.
+//  Copyright © 2018年 Shota HAMADA. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
