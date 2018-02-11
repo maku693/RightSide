c4946943b7a232e918c610bee674dcbf781276a9
Shota HAMADA <maku693@outlook.com>
2018-02-11 19:19:13 +0900

    Add `Open` window

---
+++ DirectoryViewer/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OpenViewController.swift
+//  DirectoryViewer
+//
+//  Created by Shota HAMADA on 2018/02/11.
+//  Copyright © 2018年 Shota HAMADA. All rights reserved.
+//
+
+import Cocoa
+
+class SplashViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+}
