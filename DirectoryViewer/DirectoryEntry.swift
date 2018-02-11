29ecee67eea020657cf2d2f0f0d2cb718c788366
Shota HAMADA <maku693@outlook.com>
2018-02-11 22:39:45 +0900

    Improve performance of showing directory contents

--- DirectoryViewer/DirectoryEntry.swift
+++ DirectoryViewer/DirectoryEntry.swift
@@ -13,13 +13,19 @@ class DirectoryEntry: NSObject {
     var url: URL
     @objc dynamic var title: String
     @objc dynamic var image: NSImage
-    @objc dynamic var children: [DirectoryEntry] = []
+    @objc dynamic var isFile: Bool
+    @objc dynamic lazy var children: [DirectoryEntry] = isFile ? [] : DirectoryEntry.entries(for: url)
 
     init(url: URL) {
         self.url = url.absoluteURL
         self.title = url.lastPathComponent
         self.image = NSWorkspace.shared.icon(forFile: url.path)
-        self.children = DirectoryEntry.entries(for: url)
+        self.isFile = true
+
+        if let resourceValues = try? url.resourceValues(forKeys: [.isDirectoryKey, .isPackageKey]),
+            resourceValues.isDirectory! && !resourceValues.isPackage! {
+            self.isFile = false
+        }
 
         super.init()
     }
@@ -29,11 +35,12 @@ class DirectoryEntry: NSObject {
 extension DirectoryEntry {
 
     static func entries(for url: URL) -> [DirectoryEntry] {
-        guard let entriesPath = try? FileManager.default.contentsOfDirectory(
+        guard let entryURLs = try? FileManager.default.contentsOfDirectory(
             at: url,
-            includingPropertiesForKeys: nil) else { return [] }
+            includingPropertiesForKeys: [.isDirectoryKey, .isPackageKey],
+            options: [.skipsHiddenFiles]) else { return [] }
 
-        return entriesPath.map { DirectoryEntry(url: $0) }
+        return entryURLs.map { DirectoryEntry(url: $0) }
     }
 
 }
